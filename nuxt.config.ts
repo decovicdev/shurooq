d747233e16794a7d76cc5b1b1c0371807294065d
solo samurai <ahmed_abdikani@hotmail.com>
2023-10-20 11:38:59 +0300

    :sparkles: complete `PWA` setup closes #13

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,18 +4,60 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt", "@nuxtjs/i18n"],
   css: ["~/assets/css/main.css"],
+  experimental: {
+    payloadExtraction: false,
+  },
   app: {
     head: {
       meta: [
         { name: "viewport", content: "width=device-width, initial-scale=1" },
       ],
       htmlAttrs: {
-        lang: "ar",
+        lang: "en",
       },
     },
   },
   pwa: {
-    /* your pwa options */
+    registerType: "autoUpdate",
+    manifest: {
+      name: "Shurooq",
+      short_name: "Shurooq",
+      theme_color: "#ffffff",
+      icons: [
+        {
+          src: "android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: "/",
+      globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 20,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: "module",
+    },
   },
   i18n: {
     vueI18n: "./i18n.config.ts",
